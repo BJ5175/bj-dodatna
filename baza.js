ac5128fd2e544d6d6c2bb5e6bcf810dd7a1bb4ae
Boris530262 <bokijenajbolji223@gmail.com>
2026-02-10 19:03:39 +0100

    Kalista izostanak 1

---
+++ baza.js
@@ -0,0 +1,22 @@
+// ==========================================
+        // DEFAULT BAZA UČENIKA
+        //{ datum: "10.2.2026", razlog: "Bolest" }
+        //===========================================
+        //{ ocjena: 3, opis: "" },
+        //{ ocjena: 2, opis: "Loš rad na satu" },  // <-- ovo je negativna
+        //{ ocjena: null, opis: "Bilješka: zaboravila pribor" }
+        // ==========================================
+        
+const baza = {
+    "Kalista.Galov": {
+        ime: "Kalista Galov",
+        izostanci: [
+            { datum: "11.2.2026", razlog: "Obiteljski razlozi" }
+        ],
+        podaci: [
+            { ocjena: null, opis: "Prvi sat 18.2.2026" },
+            
+        ],
+        vladanje: 5
+    }
+};
