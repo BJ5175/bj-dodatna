565db2ee0b07aabaddace8ef4cf5ed4805d5d0ca
Boris530262 <bokijenajbolji223@gmail.com>
2026-02-14 14:12:46 +0100

    ocjena

--- baza.js
+++ baza.js
@@ -1,22 +1,21 @@
 // ==========================================
-        // DEFAULT BAZA UČENIKA
-        //{ datum: "10.2.2026", razlog: "Bolest" }
-        //===========================================
-        //{ ocjena: 3, opis: "" },
-        //{ ocjena: 2, opis: "Loš rad na satu" },  // <-- ovo je negativna
-        //{ ocjena: null, opis: "Bilješka: zaboravila pribor" }
-        // ==========================================
-        
+// DEFAULT BAZA UČENIKA
+//{ datum: "10.2.2026", razlog: "Bolest" }
+//===========================================
+//{ ocjena: 3, opis: "" },
+//{ ocjena: 2, opis: "Loš rad na satu" },  // <-- ovo je negativna
+//{ ocjena: null, opis: "Bilješka: zaboravila pribor" }
+// ==========================================
+
 const baza = {
     "Kalista.Galov": {
         ime: "Kalista Galov",
         izostanci: [
-            
+
         ],
         podaci: [
             { ocjena: null, opis: "Bilješka: zaboravila pribor" },
-            { ocjena: null, opis: "Bilješka: Kalistin prvi petnaestominutni čas nije dobro prošao jer je kalista ometala predavanje i odbija slušati što joj se kaže. " },
-            { ocjena: 1, opis: "Usmeni odgovor" },
+            { ocjena: null, opis: "Bilješka: Kalistin prvi petnaestominutni čas nije dobro prošao jer je Kalista ometala predavanje i odbija slušati što joj se kaže. " },
         ],
         vladanje: 4
     }
